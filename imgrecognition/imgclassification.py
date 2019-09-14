1dcc073e32071cc57ee4267d4cb7ce682d277aaa
Seung Je Jung <xenoframium@gmail.com>
2019-09-14 12:33:57 -0400

    Add model persistence

--- imgrecognition/imgclassification.py
+++ imgrecognition/imgclassification.py
@@ -1,19 +1,45 @@
 #!/usr/bin/env python
-
-##############
-#### Your name: Seung Je Jung
-##############
-
 import numpy as np
+import os.path
+import pickle
 import re
 from sklearn import svm, metrics
 from skimage import io, feature, filters, exposure, color
 from skimage import transform
 
 class ImageClassifier:
-    
-    def __init__(self):
+    def __init__(self, modelPath = None):
         self.classifier = None
+        if modelPath != None:
+            self.load_classifier(modelPath)
+
+    def load_classifier(self, filePath):
+        file = open(filePath, "rb")
+        serialModel = file.read()
+        self.classifier = pickle.loads(serialModel)
+
+    def save_classifier(self, filePath):
+        file = open(filePath, "wb")
+        serialModel = pickle.dumps(self.classifier)
+        file.write(serialModel)
+
+    def extract_image_features(self, data):
+        feature_data = []
+        for image in data:
+            image = color.rgb2gray(image)
+            image = transform.rescale(image, 1.0 / 4.0, multichannel = False)
+            feature_data.append(feature.hog(image))
+        
+        return(feature_data)
+
+    def train_classifier(self, train_data, train_labels):
+        # train model and save the trained model to self.classifier
+        self.classifier = svm.SVC(kernel = 'poly', gamma = 'scale', degree = 2);
+        self.classifier.fit(train_data, train_labels)
+
+    def predict_labels(self, data):
+        predicted_labels = self.classifier.predict(data)
+        return predicted_labels
 
     def imread_convert(self, f):
         return io.imread(f).astype(np.uint8)
@@ -32,45 +58,13 @@ class ImageClassifier:
             labels[i] = f[len(dir):m.start()]
         
         return(data,labels)
-
-    def extract_image_features(self, data):
-        # Please do not modify the header above
-
-        # extract feature vector from image data
-        feature_data = []
-        for image in data:
-        	image = color.rgb2gray(image)
-        	image = transform.rescale(image, 1.0 / 4.0, multichannel = False)
-        	feature_data.append(feature.hog(image))
-        
-        # Please do not modify the return type below
-        return(feature_data)
-
-    def train_classifier(self, train_data, train_labels):
-        # Please do not modify the header above
-        
-        # train model and save the trained model to self.classifier
-        self.classifier = svm.SVC(kernel = 'poly', gamma = 'scale', degree = 2);
-        self.classifier.fit(train_data, train_labels)
-
-    def predict_labels(self, data):
-        # Please do not modify the header
-
-        # predict labels of test data using trained model in self.classifier
-        # the code below expects output to be stored in predicted_labels
-        
-        predicted_labels = self.classifier.predict(data)
-        
-        # Please do not modify the return type below
-        return predicted_labels
-
       
 def main():
 
     img_clf = ImageClassifier()
 
     # load images
-    (train_raw, train_labels) = img_clf.load_data_from_folder('./train/')
+    (train_raw, train_labels) = img_clf.load_data_from_folder("./train/")
     (test_raw, test_labels) = img_clf.load_data_from_folder('./test/')
     
     # convert images into features
@@ -79,6 +73,8 @@ def main():
     
     # train model and test on training data
     img_clf.train_classifier(train_data, train_labels)
+    img_clf.save_classifier("model.txt")
+
     predicted_labels = img_clf.predict_labels(train_data)
     print("\nTraining results")
     print("=============================")
