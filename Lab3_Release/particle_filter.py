e6f33c633cbd6cd4d5cc8718c2f99be3ac328629
Ishan Arya <aryaishn@gmail.com>
2019-10-01 04:29:43 -0400

    have to resample measurement update

--- Lab3_Release/particle_filter.py
+++ Lab3_Release/particle_filter.py
@@ -1,7 +1,7 @@
 from grid import *
 from particle import Particle
 from utils import *
-import setting
+from setting import *
 import numpy as np
 import math
 import random
@@ -22,9 +22,15 @@ def motion_update(particles, odom):
     """
 
     motion_particles = []
+    print(particles)
     (dx, dy, dh) = odom
+    if dx == 0 and dy == 0 and dh == 0:
+        return particles
     rad = -math.radians(dh)
     for particle in particles:
+        # noisy_odom = add_odometry_noise(
+        #     odom, ODOM_HEAD_SIGMA, ODOM_TRANS_SIGMA)
+        # dx, dy, dh = noisy_odom
         x = particle.x
         y = particle.y
         a = particle.h
@@ -44,8 +50,10 @@ def changeBasis(particle, relativeCoord):
     globY = rotY + particle.y
     return (globX, globY)
 
+
 def measurement_update(particles, measured_marker_list, grid):
-    """ Particle filter measurement update
+    """ 
+    Particle filter measurement update
 
         Arguments:
         particles -- input list of particle represents belief \tilde{p}(x_{t} | u_{t})
@@ -69,4 +77,50 @@ def measurement_update(particles, measured_marker_list, grid):
                 after measurement update
     """
     measured_particles = []
+
+    weights = [1.0] * len(particles)
+
+    for i, particle in enumerate(particles):
+        if not grid.is_in(particle.x, particle.y):
+            weights[i] = 0
+            continue
+        particleMarkers = particle.read_markers(grid)
+
+        if len(particleMarkers) == 0 and len(measured_marker_list) == 0:
+            weights[i] = 1
+        elif len(particleMarkers) == 0 or len(measured_marker_list) == 0:
+            weights[i] = DETECTION_FAILURE_RATE * SPURIOUS_DETECTION_RATE
+        else:
+            for robotMarker in measured_marker_list:
+                minDistance = float('inf')
+                diffAngle = None
+
+                rmX, rmY, rmH = robotMarker
+
+                for particleMarker in particleMarkers:
+                    pmX, pmY, pmH = particleMarker
+                    currentDistance = grid_distance(rmX, rmY, pmX, pmY)
+                    if currentDistance < minDistance:
+                        minDistance = currentDistance
+                        diffAngle = diff_heading_deg(rmH, pmH)
+
+                power = ((minDistance ** 2)/(2 * (MARKER_TRANS_SIGMA ** 2))) + \
+                    ((diffAngle ** 2)/(2 * (MARKER_ROT_SIGMA ** 2)))
+                weights[i] *= math.exp(power)
+
+        markerDifference = abs(len(particleMarkers) -
+                               len(measured_marker_list))
+
+        if len(particleMarkers) > len(measured_marker_list):
+            scale = DETECTION_FAILURE_RATE ** markerDifference
+            weights[i] *= scale
+        elif len(particleMarkers) < len(measured_marker_list):
+            scale = SPURIOUS_DETECTION_RATE ** markerDifference
+            weights[i] *= scale
+
+    sumOfWeights = sum(weights)
+    weights = map(lambda x: x / sumOfWeights, weights)
+
+    # TODO resampling
+
     return measured_particles
